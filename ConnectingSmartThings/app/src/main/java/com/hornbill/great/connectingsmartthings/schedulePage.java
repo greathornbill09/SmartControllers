e842418bde3def3e009f1379fc3d190d439b1553
Sathya Narayanan <great.hornbill09@gmail.com>
2017-06-14 21:17:57 +0530

    Adding caldroid library to the schedule view.

--- ConnectingSmartThings/app/src/main/java/com/hornbill/great/connectingsmartthings/schedulePage.java
+++ ConnectingSmartThings/app/src/main/java/com/hornbill/great/connectingsmartthings/schedulePage.java
@@ -1,27 +1,31 @@
 package com.hornbill.great.connectingsmartthings;
 
 import android.content.Context;
+import android.icu.util.Calendar;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import com.caldroid.*;
+import com.roomorama.caldroid.CaldroidFragment;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link schedulePage.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link schedulePage#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class schedulePage extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_schedule_page, container, false);
+
+        View root = inflater.inflate(R.layout.fragment_schedule_page,container,false);
+        CaldroidFragment mcaldroidFragment =  new CaldroidFragment();
+        Bundle args = new Bundle();
+        args.putInt(CaldroidFragment.START_DAY_OF_WEEK,CaldroidFragment.MONDAY);
+        mcaldroidFragment.setArguments(args);
+        getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.cal_container, mcaldroidFragment).commit();
+
+        return  root;
     }
 }

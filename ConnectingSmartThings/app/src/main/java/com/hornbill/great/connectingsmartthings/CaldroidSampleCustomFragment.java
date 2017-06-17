9be7ff091b698f921042f0e0f106aa9ad64f390d
Sathya Narayanan <great.hornbill09@gmail.com>
2017-06-17 11:36:26 +0530

    Adding text view and custom layout for caldroid calendar in order to display our schedules.

---
+++ ConnectingSmartThings/app/src/main/java/com/hornbill/great/connectingsmartthings/CaldroidSampleCustomFragment.java
@@ -0,0 +1,19 @@
+package com.hornbill.great.connectingsmartthings;
+
+/**
+ * Created by sangee on 6/17/2017.
+ */
+
+import com.roomorama.caldroid.CaldroidFragment;
+import com.roomorama.caldroid.CaldroidGridAdapter;
+
+public class CaldroidSampleCustomFragment extends CaldroidFragment {
+
+    @Override
+    public CaldroidGridAdapter getNewDatesGridAdapter(int month, int year) {
+        // TODO Auto-generated method stub
+        return new CaldroidSampleCustomAdapter(getActivity(), month, year,
+                getCaldroidData(), extraData);
+    }
+
+}
